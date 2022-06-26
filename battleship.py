f47411c0c7556ab6abc3421a25759ad82b4bd19b
Samuel Junior Candela Pomé <byfyos72@gmail.com>
2022-06-25 20:39:53 -0500

    first work on classes

--- battleship.py
+++ battleship.py
@@ -191,3 +191,136 @@ def run_game():
 
 run_game()
 
+
+
+# CLASSES - STILL IN PROGRESS
+
+class Player:
+    def __init__(self, name):
+        self.name = name
+    
+    def position_ship(self, start_pos, finish_pos):
+        # START
+        letter_start = start_pos[0].upper()
+        number_start = start_pos[1:]
+        row_start = int("".join(number_start)) - 1
+        column_start = letters_to_numbers[letter_start] + 1
+
+        # FINISH
+        letter_finish = finish_pos[0].upper()
+        number_finish = finish_pos[1:]
+        row_finish = int("".join(number_finish)) - 1
+        column_finish = letters_to_numbers[letter_finish] + 1
+
+        if letter_start == letter_finish:
+            for i in range(row_start, row_finish + 1, 1):
+                board_player[i][column_start] = "|O|"
+            
+        elif row_start == row_finish:
+            for i in range(column_start, column_finish + 1, 1):
+                board_player[row_start][i] = "|O|"
+
+    def setup_board(self):
+        print("Let's start by setting your ships!")
+        print("You have 4 ships, their sizes in blocks are 2, 3, 4 and 5! Set them out the best you can!")
+        print("In order to setup a ship you will need to pass the start block and the end block!")
+        print("Here is how the board will look like so you have an idea!")
+        print_board(board_player)
+            
+        for length in ships_lengths:
+            print("Let's set up the " + str(length) +" block long ship, type to starting block!")
+            starting_block = input()
+            print("Great work, type the finishing block!")
+            finishing_block = input()
+            print(str(length) + " block ship position -> " + starting_block + ":" + finishing_block)
+            self.position_ship(str(starting_block), str(finishing_block))
+
+    def shoot(self, position):
+        letter = position[0].upper()
+        number = position[1:]
+        row = int("".join(number)) - 1
+        column = letters_to_numbers[letter] + 1
+
+        if board_computer_hidden[row][column] == "|O|":
+            board_computer_hidden[row][column] = "|X|"
+            board_computer_displayed[row][column] = "|X|"
+        elif board_computer_hidden[row][column] == "|X|":
+            board_computer_hidden[row][column] = "|X|"
+            board_computer_displayed[row][column] = "|X|"
+        else:
+            board_computer_hidden[row][column] = "|-|"
+            board_computer_displayed[row][column] = "|-|"
+
+    def check_left_ships(self):
+        blocks_left = 0
+        for square in board_player:
+            for sub_square in square:
+                if sub_square == "|O|":
+                    blocks_left += 1
+        
+        return blocks_left
+
+class Computer:
+    def __init__(self):
+        pass
+
+    def position_ship(self, ship_size):
+        row_start = random.randint(0, 7)
+        column_start = random.randint(1, 8)
+        
+        while row_start > 7 - ship_size + 1 or column_start > 8 - ship_size + 1:
+            row_start = random.randint(0, 7)
+            column_start = random.randint(1, 8)
+            if row_start <= 7 - ship_size + 1 or column_start <= 8 - ship_size + 1:
+                break
+        
+        if row_start <= 7 - ship_size + 1 and column_start <= 8 - ship_size + 1:
+            direction = random.randint(0, 1)
+            # COLUMN = 0 | ROW = 1
+            if direction == 0:
+                for i in range(row_start, row_start + ship_size, 1):
+                    board_computer_hidden[i][column_start] = "|O|"
+            elif direction == 1:
+                for i in range(column_start, column_start + ship_size, 1):
+                    board_computer_hidden[row_start][i] = "|O|"  
+        elif column_start > row_start:
+            for i in range(row_start, row_start + ship_size, 1):
+                board_computer_hidden[i][column_start] = "|O|"
+        elif row_start > column_start:
+            for i in range(column_start, column_start + ship_size, 1):
+                board_computer_hidden[row_start][i] = "|O|"
+        
+        print(row_start, column_start)
+
+    def setup_board(self):
+        self.position_ship(2)
+        self.position_ship(3)
+        self.position_ship(4)
+        self.position_ship(5)
+
+    def shoot(self):
+        row = random.randint(0, 7)
+        column = random.randint(1, 8)
+
+        while board_player[row][column] == "|-|":
+            row = random.randint(0, 7)
+            column = random.randint(1, 8)
+
+        if board_player[row][column] == "|O|":
+            board_player[row][column] = "|X|"
+        elif board_player[row][column] == "|X|":
+            board_player[row][column] = "|X|"
+        else:
+            board_player[row][column] = "|-|"
+
+    def check_left_ships(self):
+        blocks_left = 0
+        for square in board_computer_hidden:
+            for sub_square in square:
+                if sub_square == "|O|":
+                    blocks_left += 1
+        
+        return blocks_left
+    
+player = Player("Samuel")
+# print(player.name)
